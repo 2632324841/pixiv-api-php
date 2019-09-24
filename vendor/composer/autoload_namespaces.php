5c034c7a5d10786439fd3b9309e8951fe24ae288
JC <2632324841@qq.com>
2019-09-24 20:47:16 +0800

    update

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'GifCreator' => array($vendorDir . '/sybio/gif-creator/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -53,11 +53,22 @@ class ComposerStaticInit5fa71ed3c2df4cbe48c9ec375a802453
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'G' => 
+        array (
+            'GifCreator' => 
+            array (
+                0 => __DIR__ . '/..' . '/sybio/gif-creator/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit5fa71ed3c2df4cbe48c9ec375a802453::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit5fa71ed3c2df4cbe48c9ec375a802453::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit5fa71ed3c2df4cbe48c9ec375a802453::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
